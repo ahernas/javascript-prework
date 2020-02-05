4961051ffe0b94b3e1fc0f91c2ad80705afa2d8a
Anna Garczarek <ania.garczarek@gmail.com>
2020-02-05 17:57:26 +0100

    Add variable playerMove in script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@ let computerMove = 'kamień';
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + playerMove + '! Wygrywam!')
